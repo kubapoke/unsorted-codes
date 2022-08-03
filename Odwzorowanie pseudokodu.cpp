3d472b85c03e421081299c9af9a9debc3b0aaf73
kubapoke <75582093+kubapoke@users.noreply.github.com>
2022-08-03 12:28:28 +0200

    Add files via upload

--- Odwzorowanie pseudokodu.cpp
+++ Odwzorowanie pseudokodu.cpp
@@ -1,11 +1,11 @@
-//Jakub Pπczka
+//Jakub Paczka
 //Odwzrorowanie pseudokodu
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-	int n;							//wprowadü n, A[1..n]
+	int n;							//wprowadz n, A[1..n]
 	cout<<"wprowadz n:\n";
 	cin>>n;
 	int A[n+1];
@@ -15,15 +15,15 @@ int main()
 
 	int B[n+1];						//B[1..n]
 	for(int i=1; i<=n; i++)			//dla i od 1 do n
-		B[i]=0;							//B[i]=0
+		B[i]=0;						//B[i]=0
 		
 	for(int i=1; i<=n; i++)			//dla i od 1 do n
-		if(A[i]<=n)						//jeúli A[i]<=n
-			B[A[i]]++;						//B[A[i]]++
+		if(A[i]<=n)					//jesli A[i]<=n
+			B[A[i]]++;				//B[A[i]]++
 			
 	int k=0;						//k=0
 	for(int i=1; i<=n; i++)			//dla i od 1 do n
-		if(B[i]==0)						//jeúli B[i]==0
-			k++;							//k++
-	cout<<"k = "<<k;				//zwrÛÊ k
+		if(B[i]==0)					//jesli B[i]==0
+			k++;					//k++
+	cout<<"k = "<<k;				//zwroc k
 }
